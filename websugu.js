407bbf2df8bd9db2814bdee710c9409a07dd4424
Abdourahamane Diarra <139383278+websugu@users.noreply.github.com>
2024-02-01 00:54:59 +0530

    Add files via upload

---
+++ websugu.js
@@ -0,0 +1,21 @@
+// NAV  START HERE
+$(document).ready(() => {
+    $('#hamburger-menu').click(() => {
+        $('#hamburger-menu').toggleClass('active')
+        $('#nav-menu').toggleClass('active')
+    })
+
+    // setting owl carousel
+
+    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+
+    $('#hero-carousel').owlCarousel({
+        items: 1,
+        dots: false,
+        loop: true,
+        nav:true,
+        navText: navText,
+        autoplay: true,
+        autoplayHoverPause: true
+    })
+})
