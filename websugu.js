67f372c48a44a225ada0b87ca47d0cb95384f72f
Abdourahamane Diarra <139383278+websugu@users.noreply.github.com>
2024-02-03 19:59:17 +0530

    Update websugu.js

--- websugu.js
+++ websugu.js
@@ -19,7 +19,8 @@ $(document).ready(() => {
         autoplayHoverPause: true
     })
 })
-// Function to update the timer
+
+ // Function to update the timer
  function updateTimer() {
     var now = new Date();
     var hours = now.getHours();
@@ -40,6 +41,8 @@ setInterval(updateTimer, 1000);
 
 // Initial call to update the timer
 updateTimer();
+
+
 // SOLUTION START HERE
 function showDetails(solutionId) {
     var detailsDiv = document.getElementById(solutionId + 'Details');
@@ -50,3 +53,42 @@ function showDetails(solutionId) {
     }
 }
 
+// PLANS START HERE
+function selectPlan(button) {
+    const plans = document.querySelectorAll('.plan');
+    const checkboxes = document.querySelectorAll('.checkbox');
+    const selectedPlanDetails = document.getElementById('selectedPlanDetails');
+
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = false;
+    });
+
+    plans.forEach(plan => {
+      plan.querySelector('button').innerText = "Sélectionner";
+    });
+
+    button.innerText = "choisie";
+    button.previousElementSibling.checked = true;
+
+    // Update checkout details
+    const planName = button.parentElement.querySelector('h2').innerText;
+    const planPrice = button.parentElement.querySelector('p').innerText;
+    const totalPrice = button.parentElement.querySelector('h3').innerText;
+    const overPrice = button.parentElement.querySelector('small').innerText;
+
+    selectedPlanDetails.innerHTML = `
+      <h1 style="font-size:26px;font-weight: 400;">${planName} :</h1><hr>
+      <h3 style="font-weight: 400;">Prix : ${planPrice}</h3>
+      <h3 style="font-weight: 400;">Nom Domaine : 1499  FCFA</h3><hr>
+      <h3 style="text-decoration: line-through;color: rgb(199, 17, 17);font-weight: 500;"> ${overPrice}<h3>
+      <h3 style="font-size:24px;font-weight: 600;">${totalPrice}</h3> 
+    `;
+  }
+
+  function checkout() {
+    const checkoutBtn = document.querySelector('.checkout button');
+    checkoutBtn.innerText = 'Chargement...';
+    setTimeout(function() {
+      window.location.href = 'https://www.google.com';
+    }, 2000); // Redirect after 2 seconds
+  }
